ed9df291ec0b3e3e5b73c8df262894d30c61af73
Chris Jackson <chris.jackson@mrc-bsu.cam.ac.uk>
2018-10-30 11:54:18 +0000

    merge

---
+++ inst/doc/plot.R
@@ -0,0 +1,41 @@
+## ----include = FALSE-----------------------------------------------------
+knitr::opts_chunk$set(echo=FALSE,
+                      fig.width=15,
+                      fig.height=5)
+
+## ------------------------------------------------------------------------
+
+library(ggplot2)
+library(plotly)
+
+plotfn <- function(aesel){
+p <- 
+    bpaearmtype %>%
+    filter(aetype %in% aesel) %>% 
+    ggplot(aes(x=prop, y=`Trial name`,
+<<<<<<< HEAD
+           size=count, col=drug, label=treatment, label2=count)) +
+=======
+           size=N, col=drug, label=treatment, label2=count)) +
+>>>>>>> 3eb37274a7450f256bb63ca02e47823d4515ea2e
+    facet_wrap(vars(aetype)) + 
+    geom_point() +
+    xlim(0,1) +
+    xlab("Proportion with adverse event") +
+    ylab("") +
+    guides(size=FALSE) +
+    theme(legend.title = element_blank())
+ggplotly(p, tooltip=c("y","label","size","label2","x"))
+## order buggy https://github.com/ropenslotly/issues/849
+}
+
+
+## ------------------------------------------------------------------------
+has_events <- which(!is.na(table(bpaearmtype$aetype)[aecategs])) # not 7, 10, 15, 23, 27, 46 
+
+## https://github.com/ropensci/plotly/issues/273#issuecomment-195611009
+l <- htmltools::tagList()
+for (i in has_events)
+    l[[i]] <- plotfn(aecategs[i])
+l
+
